76f50511252dd87ce38ae8c2f2f07f0db54aa82b
shwet-aaa <96437606+shwet-aaa@users.noreply.github.com>
2023-06-11 02:53:06 +0530

    Update config.js

--- src/services/config.js
+++ src/services/config.js
@@ -1,11 +1,11 @@
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  databaseURL: import.meta.env.VITE_FIREBASE_DB_URL,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MSG_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID
+  apiKey: "AIzaSyAo3wxQdvoCJGQ0-CtwD64QL2wr2nOhQYk",
+  authDomain: "e-commerce-53885.firebaseapp.com",
+  databaseURL: "https://yourdburl.firebaseio.com",
+  projectId: "e-commerce-53885",
+  storageBucket: "e-commerce-53885.appspot.com",
+  messagingSenderId: "1026647026531",
+  appId: "1:1026647026531:web:b7ddcf09b36f0e49ea4156"
 };
 
 export default firebaseConfig;
