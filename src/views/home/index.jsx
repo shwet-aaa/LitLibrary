26269a437a1e8615e2956bc7435fa5f760d6897b
shwet-aaa <lcb2021019@iiitl.ac.in>
2023-06-11 01:42:07 +0530

    First commit

---
+++ src/views/home/index.jsx
@@ -0,0 +1,93 @@
+import { ArrowRightOutlined } from '@ant-design/icons';
+import { MessageDisplay } from '@/components/common';
+import { ProductShowcaseGrid } from '@/components/product';
+import { FEATURED_PRODUCTS, RECOMMENDED_PRODUCTS, SHOP } from '@/constants/routes';
+import {
+  useDocumentTitle, useFeaturedProducts, useRecommendedProducts, useScrollTop
+} from '@/hooks';
+import bannerImg from '@/images/books.jpg';
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+
+const Home = () => {
+  useDocumentTitle('LitLibrary | Home');
+  useScrollTop();
+
+  const {
+    featuredProducts,
+    fetchFeaturedProducts,
+    isLoading: isLoadingFeatured,
+    error: errorFeatured
+  } = useFeaturedProducts(6);
+  const {
+    recommendedProducts,
+    fetchRecommendedProducts,
+    isLoading: isLoadingRecommended,
+    error: errorRecommended
+  } = useRecommendedProducts(6);
+
+  return (
+    <main className="content">
+      <div className="home">
+        <div className="banner">
+          <div className="banner-desc">
+            <h1 className="text-thin">
+              <strong>Uncover</strong>
+              &nbsp;Worlds Between&nbsp;
+              <strong>Pages</strong>
+              {/* Uncover Worlds Between Pages */}
+            </h1>
+            <h2>
+            Words that Captivate. Pages that Inspire. Books for Every Desire
+            </h2>
+            <br />
+            <Link to={SHOP} className="button">
+              Shop Now &nbsp;
+              <ArrowRightOutlined />
+            </Link>
+          </div>
+          <div className="banner-img"><img src={bannerImg} alt="" /></div>
+        </div>
+        <div className="display">
+          <div className="display-header">
+            <h1>Featured Books</h1>
+            <Link to={FEATURED_PRODUCTS}>See All</Link>
+          </div>
+          {(errorFeatured && !isLoadingFeatured) ? (
+            <MessageDisplay
+              message={errorFeatured}
+              action={fetchFeaturedProducts}
+              buttonLabel="Try Again"
+            />
+          ) : (
+            <ProductShowcaseGrid
+              products={featuredProducts}
+              skeletonCount={6}
+            />
+          )}
+        </div>
+        <div className="display">
+          <div className="display-header">
+            <h1>Recommended Books</h1>
+            <Link to={RECOMMENDED_PRODUCTS}>See All</Link>
+          </div>
+          {(errorRecommended && !isLoadingRecommended) ? (
+            <MessageDisplay
+              message={errorRecommended}
+              action={fetchRecommendedProducts}
+              buttonLabel="Try Again"
+            />
+          ) : (
+            <ProductShowcaseGrid
+              products={recommendedProducts}
+              skeletonCount={6}
+            />
+          )}
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default Home;
